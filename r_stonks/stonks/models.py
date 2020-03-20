f1ad4c6eb8643e3043ec4841526254ec447ce53d
Jonathan Hrovat <jmhrovat@gmail.com>
2020-03-20 09:59:48 -0700

    New fields

--- r_stonks/stonks/models.py
+++ r_stonks/stonks/models.py
@@ -3,3 +3,6 @@ from django.db import models
 # Create your models here.
 class Portfolio(models.Model):
     title = models.CharField(max_length=100)
+    date = models.DateField()
+    cash = models.IntegerField()
+    holdings = JSONField()
