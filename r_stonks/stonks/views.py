b706d475724592b6258765606c3659968341408e
Jonathan Hrovat <jmhrovat@gmail.com>
2020-03-20 14:33:09 -0700

    JSON Works

--- r_stonks/stonks/views.py
+++ r_stonks/stonks/views.py
@@ -1,8 +1,15 @@
 from django.http import HttpResponse
+from stonks.models import *
 
 
 def index(request):
 
+    Portfolio.objects.create(
+        title='This got created!',
+        date='2020-01-01',
+        cash=10000,
+        holdings={'AAPL': 4}
+        )
     print("This printed!")
 
     return HttpResponse("Check the terminal")
