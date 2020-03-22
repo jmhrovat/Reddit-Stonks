34a217ca1251d3ba5294767f3540fe9a4105138f
Jonathan Hrovat <jmhrovat@gmail.com>
2020-03-22 16:50:15 -0700

    Validate User Request object

--- r_stonks/modules/stonks.py
+++ r_stonks/modules/stonks.py
@@ -1,26 +1,38 @@
 class UserRequest():
 
-    def __init__(self, user_message):
-        self.user_message = user_message
+    def __new__(cls, user_message):
 
-        def is_valid_request(self):
-            parameters = self.user_message.split()
+        def is_valid_request(user_message):
+            if user_message.count("$") >=2:
+                first_index = user_message.find("$")
+                second_index = user_message.find("$", first_index + 1)
+                request = user_message[first_index:second_index + 1]
+            else:
+                return False
+
+            parameters = request.split()
+
             if (len(parameters) == 3
                 and parameters[0][0] == "$"
                 and parameters[2][0].upper() == "X"
                 and parameters[2][-1] == "$"):
                     return True
+
             else:
                 return False
 
-        try:
-            if is_valid_request(self):
-                parameters = self.user_message.split()
-                self.action = parameters[0].upper()
-                self.ticker = parameters[1].upper()
-                self.volume = int(parameters[2][1:-1])
-            else:
-                raise ValueError
-        except ValueError:
-            print('''The message provided by the user does not match the format.
-            Please double check your entry and make sure you follow the format $ACTION TICKER X###$''')
+        if is_valid_request(user_message):
+            return super(UserRequest, cls).__new__(cls)
+
+
+    def __init__(self, user_message):
+
+        first_index = user_message.find("$")
+        second_index = user_message.find("$", first_index + 1)
+        request = user_message[first_index:second_index + 1]
+
+        parameters = request.split()
+
+        self.action = parameters[0].upper()
+        self.ticker = parameters[1].upper()
+        self.volume = int(parameters[2][1:-1])
