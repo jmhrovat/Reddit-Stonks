b8b278ce5197cfc2c6ce5d5b6de146d80f0abad9
Jonathan Hrovat <jmhrovat@gmail.com>
2020-03-23 19:37:24 -0700

    Update views.py

--- r_stonks/stonks/views.py
+++ r_stonks/stonks/views.py
@@ -3,45 +3,58 @@ from modules.stonks import *
 from modules.stonks_bot import *
 from stonks.models import *
 import yfinance as yf
+from datetime import date
 
 
 def index(request):
 
     portfolio = Portfolio.objects.get(pk=1)
 
-    portfolio.reset()
+    # portfolio.reset()
 
     bot = get_stonks_bot()
 
     # Migrate url as attribute to Portfolio model
-    submission = bot.submission(url='https://www.reddit.com/r/test/comments/fl3jle/test_post/')
+    post = bot.submission(url='https://www.reddit.com/r/test/comments/fl3jle/test_post/')
 
-    user_requests = []
+    today = date.today()
+    print(today.strftime("%m/%d/%Y"))
+    post_text = "Ticker| Price | Quantity\n:--|:--:|--:\nAAPL| 100 | 50"
 
-    for top_level_comment in submission.comments:
-        user_message = top_level_comment.body
-        user_requests.append(UserRequest(user_message))
 
-    for user_request in user_requests:
-        try:
-            if user_request.action == "$BUY":
-                portfolio.buy_stock(user_request)
-            elif user_request.action == "$SELL":
-                portfolio.sell_stock(user_request)
-        except AttributeError as err:
-            pass
+
+
+
+    post.edit(post_text)
+
+    print("Edit")
+
+    # user_requests = []
+
+    # for top_level_comment in submission.comments:
+    #     user_message = top_level_comment.body
+    #     user_requests.append(UserRequest(user_message))
+    #
+    # for user_request in user_requests:
+    #     try:
+    #         if user_request.action == "$BUY":
+    #             portfolio.buy_stock(user_request)
+    #         elif user_request.action == "$SELL":
+    #             portfolio.sell_stock(user_request)
+    #     except AttributeError as err:
+    #         pass
 
     # stock = yf.Ticker(ticker)
     # closing_price = stock.history(period="today")['Close'][0]
 
 
-    print("Cash balance: ", portfolio.cash)
-    print("--------------------------------")
-    if portfolio.holdings == {}:
-        print("You have no holdings right now.")
-    else:
-        print("Holdings:")
-        print(portfolio.holdings)
+    # print("Cash balance: ", portfolio.cash)
+    # print("--------------------------------")
+    # if portfolio.holdings == {}:
+    #     print("You have no holdings right now.")
+    # else:
+    #     print("Holdings:")
+    #     print(portfolio.holdings)
 
     # print("You have the following stock:")
     # for key, value in portfolio.holdings:
